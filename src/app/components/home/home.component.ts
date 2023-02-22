d03ccdcacede701387c6a4689b33c7fe3cf987b2
VinayPsych <vinaychoudhary994@gmail.com>
2023-02-22 16:26:43 +0530

    Time Updated

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
    // Store the randomValues of Functions
    randomValue1: number = 0;
    randomValue2: number = 0;
@@ -21,40 +21,62 @@ export class HomeComponent implements OnInit {
 
   // To Calculate the Time of User
   startTime: number = 0;
-  endTime: number = 0;
-  averageTime: number = 0;
-  times: number[] = [];
+  totalTime: number;
+  numSolutions: number;
+  averageTime: number;
+  timer: any;
 
-  constructor( ) {}
+  constructor( ) {
+    this.numSolutions = 0;
+    this.totalTime = 0;
+    this.averageTime = 0;
+  }
   
   // When the Component is Loaded 
   ngOnInit() {
     this.generateEquation();
+    this.startTimer();
     this.isCorrect = false;
   }
 
-  //  we generate a new math addition equation, clear the user's answer, and reset the isCorrect and isWrong variables.
+  ngOnDestroy() {
+    this.stopTimer();
+  }
+
+  // we generate a new random addition equation and reset the appropriate variables.
   generateEquation() {
     this.randomValue1 = Math.floor(Math.random() * 10);
     this.randomValue2 = Math.floor(Math.random() * 10);
     this.sum = this.randomValue1 + this.randomValue2;
     this.isCorrect = false;
     this.userSum = "";
+    this.numSolutions++;
+  }
+
+  // we start a timer that updates the averageTime variable every second.
+  startTimer() {
+    this.startTime = Date.now();
+    this.timer = setInterval(() => {
+      this.averageTime = this.totalTime / this.numSolutions;
+    }, 1000);
+  }
+
+  // we stop the timer when the component is destroyed.
+  stopTimer() {
+    clearInterval(this.timer);
   }
 
   // we check if the user's answer is correct or not. If it's correct, we set the isCorrect variable to true, calculate the time taken to solve the Equation
   checkAnswer() {
-    this.endTime = Date.now();
-    const timeTaken = (this.endTime - this.startTime) / 1000;
-    this.times.push(timeTaken);
-    this.averageTime = this.times.reduce((a, b) => a + b, 0) / this.times.length;
     if (+this.userSum === this.sum) {
       this.isCorrect = true;
+      this.totalTime += (Date.now() - this.startTime)/ 1000;
       setTimeout(() => {
         this.generateEquation();
+        this.startTimer();
       }, 1000);
     } else {
-      this.startTime = Date.now();
+      this.isCorrect = false;
     }
   }
 }
